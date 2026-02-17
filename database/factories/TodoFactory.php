44eb5c35a8a8914ef46fd331dc1de10b10e16617
luxao <lukas.lobll@gmail.com>
2026-02-17 22:43:23 +0100

    test: add factories for user and todo

--- database/factories/TodoFactory.php
+++ database/factories/TodoFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,10 @@ class TodoFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title' => fake()->sentence(2),
+            'description' => fake()->paragraph(),
+            'completed' => fake()->boolean(),
+            'user_id' => User::factory(),
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Todo;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,11 +16,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $user = User::factory()->create([
+            'name' => 'Jozef Mrkva',
+            'email' => 'jozef.mrkva@example.com',
+        ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        Todo::factory()->count(25)->create([
+            'user_id' => $user->id,
         ]);
     }
 }
